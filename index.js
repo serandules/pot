5942c1252dcb43f058585038a6fc7d72cb414ce7
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2017-04-14 15:55:56 +0530

    updating

---
+++ index.js
@@ -0,0 +1,45 @@
+var log = require('logger')('pot');
+var nconf = require('nconf').argv().env();
+var async = require('async');
+var mongoose = require('mongoose');
+var initializers = require('initializers');
+var server = require('server');
+
+mongoose.Promise = global.Promise;
+
+var env = nconf.get('env');
+
+nconf.defaults(require('./env/' + env + '.json'));
+
+exports.start = function (done) {
+    var mongodbUri = nconf.get('mongodbUri');
+    mongoose.connect(mongodbUri);
+    var db = mongoose.connection;
+    db.on('error', function (err) {
+        log.error('mongodb connection error: %e', err);
+    });
+    db.once('open', function () {
+        log.info('connected to mongodb : ' + mongodbUri);
+        mongoose.connection.db.dropDatabase(function (err) {
+            if (err) {
+                return done(err);
+            }
+            initializers.init(function (err) {
+                if (err) {
+                    return done(err);
+                }
+                server.start(done);
+            });
+        });
+    });
+};
+
+exports.stop = function (done) {
+    server.stop(function () {
+        mongoose.disconnect(done);
+    });
+};
+
+exports.resolve = function (path) {
+    return 'http://test.serandives.com:4000' + path;
+};
